f25c9f644dbb9074fb26d058e91a032d7cd13c19
sandesh101 <rimal.sandesh11@gmail.com>
2025-03-13 23:39:40 +0545

    Semester controller

--- src/controllers/semester.controller.js
+++ src/controllers/semester.controller.js
@@ -40,15 +40,67 @@ export const createSemester = async (req, res) => {
 
 //Get semester
 export const getSemester = async (req, res) => {
+    const { collegeId } = req.params;
 
+    try {
+        // Find semesters where collegeId matches the provided collegeId
+        const semesters = await Semester.find({ collegeId });
+
+        // If no semesters are found
+        if (!semesters || semesters.length === 0) {
+            return res.status(404).json(new ApiResponse(404, '', 'Unable to find semesters for the specified college'));
+        }
+
+        // Return the found semesters
+        return res.status(200).json(new ApiResponse(200, semesters, 'Success'));
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(new ApiResponse(500, '', 'Error fetching semesters'));
+    }
 }
 
 //Update semester
 export const updateSemester = async (req, res) => {
+    const { name } = req.body;
+    const { id } = req.params;
 
+    try {
+        //Check if field empty
+        if (!name) {
+            return res.status(400).json(new ApiResponse(400, "", 'Semester name is required'));
+        }
+
+        //Check if semester available
+        const semester = await Semester.findById(id);
+        if (!semester) {
+            return res.status(404).json(new ApiResponse(404, "", 'No semester foudn'));
+        }
+
+        if (name) {
+            semester.name = name;
+        }
+        await semester.save();
+        return res.status(200).json(new ApiResponse(200, semester, 'Update Successfull'));
+    } catch (error) {
+        return res.status(500).json(new ApiResponse(500, "", 'Error updating'));
+    }
 }
 
 //Delete semester
 export const deleteSemester = async (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    try {
+        //Find the provided semester
+        const semester = await Semester.findById(id);
 
+        if (!semester) {
+            return res.status(404).json(new ApiResponse(404, '', 'Unable to find semester'));
+        }
+
+        await semester.deleteOne();
+        return res.status(200).json(new ApiResponse(200, "", 'Deleting semester successfull'));
+    } catch (error) {
+        return res.status(500).json(new ApiResponse(500, "", 'Error deleting'));
+    }
 }
--- src/routes/semester.route.js
+++ src/routes/semester.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { createSemester } from '../controllers/semester.controller.js';
+import { createSemester, getSemester, deleteSemester, updateSemester } from '../controllers/semester.controller.js';
 import { validateToken } from '../middlewares/verifyToken.js';
 
 const router = express.Router();
 
 router.route('/create').post(validateToken, createSemester);
+router.route('/get-semester/:collegeId').get(validateToken, getSemester);
+router.route('/update/:id').put(validateToken, updateSemester);
+router.route('/delete/:id').delete(validateToken, deleteSemester);
 
 
 export default router;
