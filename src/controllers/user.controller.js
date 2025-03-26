a7b8c041f7abb46674ccc00bec0fa19532735c74
sandesh101 <rimal.sandesh11@gmail.com>
2025-03-26 22:55:37 +0545

    Fix: user data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,145 +1,171 @@
 import { ApiResponse } from "../utils/ApiResponse.js";
 import User from "../models/user.model.js";
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import BlacklistToken from '../models/blacklisttoken.model.js';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import BlacklistToken from "../models/blacklisttoken.model.js";
 
 //Register user controller
 export const registerUser = async (req, res) => {
-    const { email, fullName, password, role, profilePicture } = req.body;
-    try {
-
-        //Check for empty fields
-        if (!email || !fullName || !password) {
-            res.status(400).json(new ApiResponse(400, "All fields are required", false));
-        }
-
-
-        //Check if user with same email already exists
-        const userAlreadyExists = await User.findOne({ email });
-        if (userAlreadyExists) {
-            res.status(400).json(new ApiResponse(400, "User with same email already exists", false));
-        }
-
-        //Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        //Creating user with hashed password
-        const user = new User({
-            fullName,
-            email,
-            password: hashedPassword,
-            role,
-            profilePicture
-        });
-
-        await user.save();
-
-        //Removing password field in response
-        const createdUser = await User.findById(user._id).select("-password");
-
-        //Check if user correctly created
-        if (!createdUser) {
-            res.status(500).json({
-                message: "Somthing went wrong while registering user"
-            })
-        }
-
-        const accessToken = await jwt.sign({
-            user: {
-                id: user._id,
-                email: user.email,
-                username: user.username,
-                mobile: user.mobile
-            },
-        },
-            process.env.ACCESS_TOKEN,
-            { expiresIn: '1h', },
-        );
-
-        //Check if user created or not
-        if (!user) {
-            res.status(500).json(new ApiResponse(500, "Something went wrong", false));
-        }
-
-        //Returning the registered user
-        return res.status(200).json(new ApiResponse(200, { token: accessToken, user: createdUser }, "Signup successful"));
-    } catch (error) {
-        console.log(error);
+  const { email, fullName, password, role, profilePicture } = req.body;
+  try {
+    //Check for empty fields
+    if (!email || !fullName || !password) {
+      res
+        .status(400)
+        .json(new ApiResponse(400, "All fields are required", false));
     }
-}
 
+    //Check if user with same email already exists
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      res
+        .status(409)
+        .json(
+          new ApiResponse(409, {}, "User with same email already exists", false)
+        );
+    }
+
+    //Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //Creating user with hashed password
+    const user = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+      role,
+      profilePicture,
+    });
+
+    await user.save();
+
+    //Removing password field in response
+    const createdUser = await User.findById(user._id).select("-password");
+
+    //Check if user correctly created
+    if (!createdUser) {
+      res.status(500).json({
+        message: "Somthing went wrong while registering user",
+      });
+    }
+
+    const accessToken = await jwt.sign(
+      {
+        user: {
+          id: user._id,
+          email: user.email,
+          username: user.username,
+          mobile: user.mobile,
+        },
+      },
+      process.env.ACCESS_TOKEN,
+      { expiresIn: "1h" }
+    );
+
+    //Check if user created or not
+    if (!user) {
+      res.status(500).json(new ApiResponse(500, "Something went wrong", false));
+    }
+
+    //Returning the registered user
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { token: accessToken, user: createdUser },
+          "Signup successful"
+        )
+      );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //Login user controller
 export const loginUser = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-        if (!user) {
-            return res.status(401).json({
-                message: "User does not exists",
-            });
-        }
-
-        const isPasswordMatched = await bcrypt.compare(password, user.password);
-        if (!isPasswordMatched) {
-            return res.status(401).json({
-                message: "Invalid Email or password",
-            });
-        } else {
-            //Sign user with jwt
-            const userWOPassword = await User.findById(user._id).select("-password");
-            const accessToken = await jwt.sign({
-                user: {
-                    id: user._id,
-                    email: user.email,
-                    username: user.username,
-                    mobile: user.mobile
-                },
-            },
-                process.env.ACCESS_TOKEN,
-                { expiresIn: '1h', },
-            );
-            return res.status(200).json(new ApiResponse(200, { token: accessToken, user: userWOPassword }, "Login successful"));
-        }
-    } catch (err) {
-        console.log(err);
+    if (!user) {
+      return res.status(401).json({
+        message: "User does not exists",
+      });
     }
-}
 
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatched) {
+      return res.status(401).json({
+        message: "Invalid Email or password",
+      });
+    } else {
+      //Sign user with jwt
+      const userWOPassword = await User.findById(user._id).select("-password");
+      const accessToken = await jwt.sign(
+        {
+          user: {
+            id: user._id,
+            email: user.email,
+            username: user.username,
+            mobile: user.mobile,
+          },
+        },
+        process.env.ACCESS_TOKEN,
+        { expiresIn: "1h" }
+      );
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            200,
+            { token: accessToken, user: userWOPassword },
+            "Login successful"
+          )
+        );
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //Logout user controller
 export const logoutUser = async (req, res) => {
-    try {
-        const token = req.token;
-        await BlacklistToken.create({ token });
+  try {
+    const token = req.token;
+    await BlacklistToken.create({ token });
 
-        res.status(200).json(new ApiResponse(200, '', "Logged out successfuly"));
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(new ApiResponse(500, "Something went wrong while logout"));
-    }
-}
+    res.status(200).json(new ApiResponse(200, "", "Logged out successfuly"));
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json(new ApiResponse(500, "Something went wrong while logout"));
+  }
+};
 
 //Get current user
 
 export const getCurrentUser = async (req, res) => {
-    try {
-        const userId = req.user?.id;
-        if (!userId) {
-            return res.status(400).json({ success: false, message: "User ID is required" });
-        }
-
-        const user = await User.findById(userId).select('-password');
-        if (!user) {
-            return res.status(404).json({ success: false, message: "User not found" });
-        }
-
-        res.status(200).json({ success: true, user });
-    } catch (error) {
-        console.error("Error fetching user:", error);
-        res.status(500).json({ success: false, message: "Server error" });
+  try {
+    const userId = req.user?.id;
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User ID is required" });
     }
-}
+
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
